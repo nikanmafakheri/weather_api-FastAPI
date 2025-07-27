41f7cbaf10b814f25ba2b52901061bc449628859
nikan <nikanmafakherii@gmail.com>
2025-07-27 11:47:46 +0330

    feat: final changes to router.py and routing

--- app/domains/weather/router.py
+++ app/domains/weather/router.py
@@ -1,11 +1,19 @@
-from fastapi import APIRouter , Query 
+from fastapi import APIRouter, Depends, Query
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.core.database import SessionLocal
+from app.domains.weather.schemas import WeatherResponse
+from app.services.weather_service import get_weather
 
 router = APIRouter()
 
-@router.get("/")
-def get_weather(city : str = Query(..., description="City name to get weather for ")):
-  return {
-  "city": city,
-  "temperature" : 30,
-  "description":"(dummy data)"
-}
+async def get_db():
+    async with SessionLocal() as session:
+        yield session
+
+@router.get("/", response_model=WeatherResponse)
+async def weather(
+    city: str = Query(..., description="City name to get weather for"),
+    db: AsyncSession = Depends(get_db),
+):
+    return await get_weather(city, db)
+
