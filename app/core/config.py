86c226ebaf3fb62460bba22c52b3ab86fe603521
nikan <nikanmafakherii@gmail.com>
2025-07-27 11:31:22 +0330

    feat: creating database connection and postgres configuration

--- app/core/config.py
+++ app/core/config.py
@@ -4,6 +4,12 @@ class settings(BaseSettings):
   OPENWEATHER_API_KEY: str
   REDIS_HOST: str
   REDIS_PORT: int  
+    
+  POSTGRES_USER = str
+  POSTGRES_PASSWORD = str 
+  POSTGRES_DB = str
+  POSTGRES_HOST = str
+  POSTGRES_PORT = int
   
   class Config:
     env_file = ".env"
